79ef2328ce9ab511a6787ea2b0ba38097873f357
Titouan Manachem <titouan.manachem@gmail.com>
2019-02-13 15:56:44 +0100

    Ajout membre dans la création d'un projet

--- src/app/projects-create/projects-create.component.ts
+++ src/app/projects-create/projects-create.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, FormArray, NgForm} from '@angular/forms';
 import { ProjectService } from '../services/project.service';
+import { UserService } from '../services/user.service';
 import {availableSubscribeMethodsInit} from '../models/availableSubscribeMethods'
+import {AuthService} from "../auth/auth.service";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-projects-create',
@@ -9,8 +12,10 @@ import {availableSubscribeMethodsInit} from '../models/availableSubscribeMethods
   styleUrls: ['./projects-create.component.scss']
 })
 export class ProjectsCreateComponent implements OnInit {
-  constructor(public fb : FormBuilder, public projectService : ProjectService) { }  
-  
+
+  constructor(public fb : FormBuilder, public projectService : ProjectService, public userService : UserService ,  public afAuth: AuthService) { }
+  creator = this.afAuth.authState;
+  memberProject =  [this.creator];
   projectForm : FormGroup;
   subMethods: any;
   ngOnInit() {
@@ -44,7 +49,49 @@ public async createProject(){
     return this.projectForm.get('availableSubscribeMethodsInit')
   }
 
+  addMemberProject(form: NgForm) {
+    const user = this.userService.getMemberByEmail(form.value['email']);
 
+    user.subscribe(res => {
+      if(res.length == 0){
+        /**
+         * Affiche ton un message d'erreur todo
+         */
+        console.log('membre innexistant');
+        return;
+      }
 
-  
+      return res.map(
+        async user => {
+          var check = 1;
+          this.memberProject.forEach(member => {
+            if(member.uid == user.uid){
+              console.log('membre déjà dans le projet');
+              check = 0;
+            }
+          });
+          if(check){
+            this.memberProject.push(user);
+          } else {
+            //TODO
+          }
+        })
+    })
+
+  }
+
+  deleteMemberProject(member){
+
+     if(member.uid == this.creator.uid){
+       console.log('impossible de supprimer le créateur d un projet');
+       return;
+     }
+     const newArray = [];
+     this.memberProject.forEach( (user) => {
+        if(member != user){
+          newArray.push(user);
+        }
+     })
+    this.memberProject = newArray;
+  }
 }
