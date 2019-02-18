47baad2b5a387955e501878620fedbf4070508fb
Alexandre Payen <alexandre@Alexandres-MacBook-Pro.local>
2019-02-18 23:18:10 +0100

    ajout de la possibilite de s'inscire avec un formulaire

--- src/app/project-subscribe/project-subscribe.component.ts
+++ src/app/project-subscribe/project-subscribe.component.ts
@@ -6,6 +6,7 @@ import { SubscribersService } from '../services/subscribers.service';
 import { OCT } from '@angular/material';
 import * as firebase from 'firebase';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-project-subscribe',
@@ -18,14 +19,30 @@ export class ProjectSubscribeComponent implements OnInit {
   public projectSub: Subscription
   public subMethods: Array<any>;
   public thanks: Boolean = false;
+  public subForm: FormGroup;
+
+
+
   constructor(
     public route: ActivatedRoute,
     public router: Router,
     public projectService: ProjectService,
     public subscribersService: SubscribersService,
-    public afStore : AngularFirestore
+    public afStore: AngularFirestore,
+    public fb: FormBuilder,
 
   ) {
+
+    this.subForm = this.fb.group({
+      displayName: ["", [
+        Validators.required,
+      ]],
+      email: ["", [
+        Validators.email,
+        Validators.required
+      ]]
+    })
+
     this.thanks = this.router.url.includes('thanks');
     firebase.auth().getRedirectResult().then((result) => {
       this.route.params.subscribe(params => {
@@ -35,23 +52,41 @@ export class ProjectSubscribeComponent implements OnInit {
           this.project = project
           if (user != null) {
             this.subscribersService.updateSub(projectDoc, user)
-          }else{
-            if(params["method"]){
-             let subMethod = this.project.availableSubscribeMethods.find(sub => sub.methodName.toLowerCase() == params["method"].toLowerCase() && sub.active)
-             if(subMethod)
-              this.subscribersService[`doSubscribe${subMethod.methodName}`]() 
+          } else {
+            if (params["method"]) {
+              let subMethod = this.project.availableSubscribeMethods.find(sub => sub.methodName.toLowerCase() == params["method"].toLowerCase() && sub.active)
+              if (subMethod)
+                this.subscribersService[`doSubscribe${subMethod.methodName}`]()
+            }
           }
-        }
         })
-        
-        
+
+
 
       })
     });
   }
 
+
+  public manualSub() {
+    this.route.params.subscribe(params => {
+      let projectDoc = this.afStore.doc<Project>(`projects/${params['id']}`)
+      this.projectService.getProject(params['id']).subscribe(project => {
+        this.subscribersService.updateSub(projectDoc, {email : this.email, displayName : this.displayName})
+      })
+    })
+  }
+
   ngOnInit() {
 
+  }
+
+  get email() {
+    return this.subForm.value.email
+  }
+
+  get displayName() {
+    return this.subForm.value.displayName
   }
 }
 
