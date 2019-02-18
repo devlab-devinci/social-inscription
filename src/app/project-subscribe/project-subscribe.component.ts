03e182f5e51ebb40adb07423de29bbbe912fca48
Alexandre Payen <alexandre@alexandres-macbook-pro.home>
2019-02-18 22:43:16 +0100

    ajout de la fonctionnalite de subscribers et du login facebook

--- src/app/project-subscribe/project-subscribe.component.ts
+++ src/app/project-subscribe/project-subscribe.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectService } from '../services/project.service';
 import { SubscribersService } from '../services/subscribers.service';
 import { OCT } from '@angular/material';
+import * as firebase from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-project-subscribe',
@@ -20,31 +22,36 @@ export class ProjectSubscribeComponent implements OnInit {
     public route: ActivatedRoute,
     public router: Router,
     public projectService: ProjectService,
-    public subscribersService: SubscribersService
+    public subscribersService: SubscribersService,
+    public afStore : AngularFirestore
 
   ) {
-
-    this.route.params.subscribe(params => {
-      this.thanks = this.router.url.includes('thanks')
-      this.projectSub = this.projectService.getProject(params['id']).subscribe(res => {
-        this.project = res;
-        
-        if (params['method']) {
-          this.doSubscribe(this.project.availableSubscribeMethods.find(sub => {
-            return sub.methodName.toLowerCase() == params['method'].toLowerCase() && sub.active
-          }))
+    this.thanks = this.router.url.includes('thanks');
+    firebase.auth().getRedirectResult().then((result) => {
+      this.route.params.subscribe(params => {
+        var user = result.user;
+        let projectDoc = this.afStore.doc<Project>(`projects/${params['id']}`)
+        this.projectService.getProject(params['id']).subscribe(project => {
+          this.project = project
+          if (user != null) {
+            this.subscribersService.updateSub(projectDoc, user)
+          }else{
+            if(params["method"]){
+             let subMethod = this.project.availableSubscribeMethods.find(sub => sub.methodName.toLowerCase() == params["method"].toLowerCase() && sub.active)
+             if(subMethod)
+              this.subscribersService[`doSubscribe${subMethod.methodName}`]() 
+          }
         }
-      });
+        })
+        
+        
+
+      })
     });
   }
 
   ngOnInit() {
 
-  }
-
-  public doSubscribe(subMethod) {
-    if (subMethod)
-      this.subscribersService[`doSubscribe${subMethod.methodName}`]();
   }
 }
 
